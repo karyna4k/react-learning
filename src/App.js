444e13aa72830c90b9972e2e49f3fb8367bd61df
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-03-01 22:44:09 +0300

    feat: lesson eight

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">End</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+function Technologies() {
+  return (
+    <ul>
+      <li>CSS</li>
+      <li>HTML</li>
+      <li>JS</li>
+      <li>React</li>
+    </ul>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      <Technologies />
     </div>
   );
 }
