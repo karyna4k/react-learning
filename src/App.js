68c5a69af1479458502fbd2988d0e8e7da3d6b55
Karyna <karyna.kananovich@gmail.com>
2021-03-02 11:18:24 +0300

    feat: lesson 12

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
 import "./App.css";
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img
-          src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/gradient_2.png"
-          alt=""
-        />
-      </header>
-      <nav className="nav">
-        <ul>
-          <li>
-            <a href="">Profile</a>
-          </li>
-          <li>
-            <a href="">Messages</a>
-          </li>
-          <li>
-            <a href="">News</a>
-          </li>
-          <li>
-            <a href="">Music</a>
-          </li>
-          <li>
-            <a href="">Settings</a>
-          </li>
-        </ul>
-      </nav>
-      <main className="content">
-        <div className="content__img">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Herjangsfjorden_%26_Ofotfjorden%2C_wide%2C_2009_09.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          ava + info
-        </div>
-        <div>
-          my post
-        </div>
-        <ul>
-          <li>post 1</li>
-          <li>post 2</li>
-        </ul>
-      </main>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+function Profile() {
+  return (
+    <main className="content">
+      <div className="content__img">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Herjangsfjorden_%26_Ofotfjorden%2C_wide%2C_2009_09.jpg"
+          alt=""
+        />
+      </div>
+      <div>ava + info</div>
+      <div>my post</div>
+      <ul>
+        <li>post 1</li>
+        <li>post 2</li>
+      </ul>
+    </main>
+  );
+}
+
+export default Profile;
