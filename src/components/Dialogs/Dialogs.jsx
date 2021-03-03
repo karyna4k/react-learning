08732b1889655bbd855681e0a0b463d8255d5466
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-03-03 19:51:02 +0300

    feat: lesson 22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,30 @@
 import styles from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 function Dialogs() {
   return (
     <div className={styles.dialogs}>
       <h2 className={styles.title}>Диалоги</h2>
       <div className={styles.wrap}>
-        <div className={styles.contactsList}>
-          <div className={`${styles.contactsItem} ${styles.active}`}>Barsik</div>
-          <div className={styles.contactsItem}>Porosenok</div>
-          <div className={styles.contactsItem}>Maria</div>
-          <div className={styles.contactsItem}>Bublik</div>
-        </div>
-        <div className={styles.messagesList}>
-          <div className={styles.messageItem}>Hi!</div>
-          <div className={styles.messageItem}>How are you?</div>
-          <div className={styles.messageItem}>Hello!</div>
-        </div>
+        <ul className={styles.contactsList}>
+          <li className={`${styles.contactsItem} ${styles.active}`}>
+            <NavLink to="/dialogs/1" activeClassName={styles.active}>Barsik</NavLink>
+          </li>
+          <li className={styles.contactsItem}>
+            <NavLink to="/dialogs/2" activeClassName={styles.active}>Marsik</NavLink>
+          </li>
+          <li className={styles.contactsItem}>
+            <NavLink to="/dialogs/3" activeClassName={styles.active}>Hrusha</NavLink>
+          </li>
+          <li className={styles.contactsItem}>
+            <NavLink to="/dialogs/4" activeClassName={styles.active}>Bublik</NavLink>
+          </li>
+        </ul>
+        <ul className={styles.messagesList}>
+          <li className={styles.messageItem}>Hi!</li>
+          <li className={styles.messageItem}>How are you?</li>
+          <li className={styles.messageItem}>Hello!</li>
+        </ul>
       </div>
     </div>
   );
